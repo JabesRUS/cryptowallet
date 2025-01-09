72622508fc3800e1fcbaff8d3fb4cd54b86531dd
jabes <jabesgames@gmail.com>
2025-01-09 18:00:52 +0300

    этап 7.1 готов

---
+++ src/test/java/com/javaacademy/cryptowallet/controller/CryptoAccountControllerTest.java
@@ -0,0 +1,14 @@
+package com.javaacademy.cryptowallet.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class CryptoAccountControllerTest {
+
+}
--- src/test/java/com/javaacademy/cryptowallet/controller/UserControllerTest.java
+++ src/test/java/com/javaacademy/cryptowallet/controller/UserControllerTest.java
@@ -19,14 +19,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
